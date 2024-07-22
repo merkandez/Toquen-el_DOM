0c819ad0ac99daef3871b82894b021db7b95d203
devegalaura-dev <lauradevega94@gmail.com>
2024-07-22 09:38:30 +0200

    Add javascript for buttons

---
+++ src/js/home-page.js
@@ -0,0 +1,13 @@
+function redirectionGuitar (){
+    location.href = "maintenance-page-guitar.html"
+}
+function redirectionViolin (){
+    location.href = "maintenance-page-violin.html"
+}
+function redirectionDrums (){
+    location.href = "maintenance-page-drums.html"
+}
+function redirectionPiano (){
+    location.href = "maintenance-page-instrument.html"
+}
+
