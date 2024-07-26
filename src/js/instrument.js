ea00ef54c69b33ef1ac6efe2b47e6d325f7209c6
jfigueira87 <jfigueira@gmail.com>
2024-07-26 08:45:02 +0200

    Add functionality to play two keys at the same time and volume

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,25 +1,45 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeControl = document.querySelector('.volume-slider input');
+const ckeckKeys = document.querySelector('.keys-checkbox input');
+let volume = volumeControl.value / 1
 
-let audio = new Audio("/public/assets/sounds/a.mp3");
-
+console.log(checkKeys)
 const playTune=(key) => {
-    audio.src=`/public/assets/sounds/${key}.mp3`;
+    const audio = new Audio(`/public/assets/sounds/${key}.mp3`);
     audio.play();
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    audio.volume=volume;
+    if (clickedKey) {
     clickedKey.classList.add("active");
     setTimeout(() => {
-        clickedKey.classList.remove("active");
-    },150)
+      clickedKey.classList.remove("active");
+    }, 150);  
+}
 }
 
+
+volumeControl.addEventListener('input', (e) => {
+    volume = e.target.value / 1;
+});
+
 pianoKeys.forEach(key =>{
-    key.addEventListener("mousedown",() => playTune(key.dataset.key));
+    key.addEventListener("click",() => playTune(key.dataset.key));
 }
 );
 
-const pressedKey = (e) =>{
-    playTune(e.key);
+const showHideKeys = () =>{
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+const pressedKey = (e) => {
+    // Asegurarse de que solo las teclas relevantes desencadenen sonidos
+    const key = e.key.toLowerCase();
+    if (key >= 'a' && key <= 'z') { // Limitar a teclas alfabéticas por ejemplo
+        playTune(key);
+    }
 }
 
 document.addEventListener("keydown", pressedKey);
+rangeVolume.addEventListener("input", handleVolume);
+ckeckKeys.addEventListener("click", showHideKeys);
 
