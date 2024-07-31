074a9dddcc703e86f145672ecb4953fb1968020a
devegalaura-dev <lauradevega94@gmail.com>
2024-07-31 19:03:35 +0200

    Modify js y html with corrects routs in home-page

--- components/header/headerHamburger.js
+++ components/header/headerHamburger.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.getElementById('hamburger');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const hamburgerIcon = 'hamburgerIcon.png';
-    const closeIcon = 'closeIcon.png';
-  
-    hamburger.addEventListener('click', function () {
-      mobileMenu.classList.toggle('hidden');
-      if (mobileMenu.classList.contains('hidden')) {
-        hamburger.src = hamburgerIcon;
-      } else {
-        hamburger.src = closeIcon;
-      }
-    });
+  const hamburger = document.getElementById('hamburger');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const hamburgerIcon = '../public/assets/ilustraciones/hamburgerIcon.png';
+  const closeIcon = '../public/assets/ilustraciones/closeIcon.png';
+
+  hamburger.addEventListener('click', function () {
+    mobileMenu.classList.toggle('hidden');
+    if (mobileMenu.classList.contains('hidden')) {
+      hamburger.src = hamburgerIcon;
+    } else {
+      hamburger.src = closeIcon;
+    }
   });
+});
