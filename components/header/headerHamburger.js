676fc76d36307e358ba7f42d1f5fd946facad6c5
jfigueira87 <jfigueira@gmail.com>
2024-07-31 19:28:51 +0200

    change image src

--- components/header/headerHamburger.js
+++ components/header/headerHamburger.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const mobileMenu = document.getElementById('mobile-menu');
-    const hamburgerIcon = 'hamburgerIcon.png';
-    const closeIcon = 'closeIcon.png';
+    const hamburgerIcon = '/public/assets/iconos-rrss/hamburgerIcon.png';
+    const closeIcon = '/public/assets/iconos-rrss/closeIcon.png';
   
     hamburger.addEventListener('click', function () {
       mobileMenu.classList.toggle('hidden');
